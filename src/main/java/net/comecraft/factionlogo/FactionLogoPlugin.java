0338fd3cb64cb368d2ce6a903c943283f23d0009
garypwn <garypwn@users.noreply.github.com>
2018-07-04 11:31:44 -0700

    Register commands on enable

--- src/main/java/net/comecraft/factionlogo/FactionLogoPlugin.java
+++ src/main/java/net/comecraft/factionlogo/FactionLogoPlugin.java
@@ -1,5 +1,9 @@
 package net.comecraft.factionlogo;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.comecraft.factionlogo.wrapper.FactionsPlugin;
@@ -7,6 +11,10 @@ import net.comecraft.factionlogo.wrapper.FactionsPlugin;
 public class FactionLogoPlugin extends JavaPlugin {
 	
 	private FactionsPlugin factions;
+	private Logos logos;
+	private FileConfiguration lang;
+	private SetLogo setLogoCommand;
+	
 	
 	@Override
 	public void onEnable() {
@@ -20,5 +28,17 @@ public class FactionLogoPlugin extends JavaPlugin {
 			getLogger().severe("[factionlogo] could not find a factions plugin. Disabling factionlogo.");
 			getServer().getPluginManager().disablePlugin(this);
 		}
+		
+		// Get resource files
+		this.lang = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "lang.yml"));
+		
+		FileConfiguration logoFile = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "logos.yml"));
+		
+		this.logos = new Logos(logoFile, factions);
+		
+		// Register set logo command
+		this.setLogoCommand = new SetLogo(lang, factions, logos);
+		getCommand("setlogo").setExecutor(setLogoCommand);
+		factions.registerSubCommand(setLogoCommand);
 	}
 }
