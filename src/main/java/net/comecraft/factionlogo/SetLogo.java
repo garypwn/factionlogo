d07f105e298da560c4bf9dc129cc74ee1bf52720
garypwn <garypwn@users.noreply.github.com>
2018-07-03 22:42:11 -0700

    Add setlogo command

---
+++ src/main/java/net/comecraft/factionlogo/SetLogo.java
@@ -0,0 +1,15 @@
+package net.comecraft.factionlogo;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SetLogo implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
