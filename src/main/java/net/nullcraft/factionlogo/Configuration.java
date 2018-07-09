7d3909a4e49e89b179b6c59dafd48878b078c113
garypwn <garypwn@users.noreply.github.com>
2018-07-08 20:40:40 -0700

    Add documentation to Configuration

--- src/main/java/net/nullcraft/factionlogo/Configuration.java
+++ src/main/java/net/nullcraft/factionlogo/Configuration.java
@@ -16,6 +16,10 @@ public class Configuration {
 	private JavaPlugin plugin;
 	private FileConfiguration lang;
 	
+	/**
+	 * Constructs a Configuration
+	 * @param plugin The plugin to get the config files for
+	 */
 	public Configuration(JavaPlugin plugin) {
 		
 		this.plugin = plugin;
@@ -28,16 +32,28 @@ public class Configuration {
 		this.lang = YamlConfiguration.loadConfiguration(langFile);
 	}
 	
+	/**
+	 * Get the pattern for a valid logo
+	 * @return A Pattern that matches valid logos
+	 */
 	public Pattern getValidLogoPattern() {
-		// TODO
-		return null;
+		return Pattern.compile(plugin.getConfig().getString("valid-logo-pattern"));
 	}
 	
+	/**
+	 * Gets a lang string from the lang file
+	 * @param path The path for the string
+	 * @return The string
+	 */
 	public String getLangString(String path) {
 		// TODO
 		return null;
 	}
 	
+	/**
+	 * Gets the command aliases to be used with /f [alias]
+	 * @return A collection containing the f command aliases.
+	 */
 	public Collection<String> getFCommandAliases() {
 		// TODO
 		return null;
